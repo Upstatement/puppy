881077bc9a9eb1fab975d059edbf7f53a2b256e2
Mike Burns <mike.burns@upstatement.com>
2019-10-11 21:01:37 -0400

    Add .travis.yml
    Closes #15

--- tests/index.js
+++ tests/index.js
@@ -1,5 +1,6 @@
 const test = require('ava');
 const vfs = require('vinyl-fs');
+const crypto = require('crypto');
 const concat = require('concat-stream');
 const through = require('through2');
 const { promisify } = require('util');
@@ -9,7 +10,7 @@ const pipeline = promisify(require('stream').pipeline);
 
 // Transform Vinyl files into a more snapshot-friendly structure.
 const jsonify = through.obj((file, enc, cb) =>
-  cb(null, { path: file.relative, contents: file.contents && file.contents.toString() }),
+  file.contents ? cb(null, { path: file.relative, contents: file.contents }) : cb(),
 );
 
 test.afterEach(() => exec('npm run clean'));
@@ -29,5 +30,18 @@ test('build for prod', async t => {
 test('snapshot files in dist', async t => {
   await exec('npm run build');
 
-  await pipeline(vfs.src('dist/**/*'), jsonify, concat(files => t.snapshot(files)));
+  await pipeline(
+    vfs.src('dist/**/*'),
+    jsonify,
+    concat(files => {
+      const hashes = files.reduce((prev, curr) => {
+        prev[curr.path] = crypto
+          .createHash('md5')
+          .update(curr.contents.toString())
+          .digest('hex');
+        return prev;
+      }, {});
+      t.snapshot(hashes);
+    }),
+  );
 });
