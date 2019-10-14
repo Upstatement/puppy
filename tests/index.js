4536e26ba44e7b2a2c51c0659c43c46666ed6647
Mike Burns <mike.burns@upstatement.com>
2019-10-14 17:13:36 -0400

    Simplify dist snapshot test

--- tests/index.js
+++ tests/index.js
@@ -1,17 +1,32 @@
 const test = require('ava');
 const vfs = require('vinyl-fs');
 const crypto = require('crypto');
-const concat = require('concat-stream');
-const through = require('through2');
 const { promisify } = require('util');
 
 const exec = promisify(require('child_process').exec);
-const pipeline = promisify(require('stream').pipeline);
 
-// Transform Vinyl files into a more snapshot-friendly structure.
-const jsonify = through.obj((file, enc, cb) =>
-  file.contents ? cb(null, { path: file.relative, contents: file.contents }) : cb(),
-);
+const md5 = str => crypto
+  .createHash('md5')
+  .update(str)
+  .digest('hex');
+
+const hashDirectory = glob =>
+  new Promise((resolve, reject) => {
+    const files = {};
+
+    vfs
+      .src(glob)
+      .on('data', file => {
+        // Transform Vinyl files into a more snapshot-friendly structure.
+        if (!file.contents) {
+          return;
+        }
+
+        files[file.relative] = md5(file.contents.toString());
+      })
+      .on('end', () => resolve(files))
+      .on('error', err => reject(err));
+  });
 
 test.afterEach(() => exec('npm run clean'));
 
@@ -29,19 +44,6 @@ test('build for prod', async t => {
 
 test('snapshot files in dist', async t => {
   await exec('npm run build');
-
-  await pipeline(
-    vfs.src('dist/**/*'),
-    jsonify,
-    concat(files => {
-      const hashes = files.reduce((prev, curr) => {
-        prev[curr.path] = crypto
-          .createHash('md5')
-          .update(curr.contents.toString())
-          .digest('hex');
-        return prev;
-      }, {});
-      t.snapshot(hashes);
-    }),
-  );
+  const distHashes = await hashDirectory('dist/**/*');
+  t.snapshot(distHashes);
 });
