ed756b830ca90612a5a825b6194aee476f4eff12
Mike Burns <mike.burns@upstatement.com>
2020-04-03 17:16:34 -0400

    Comment out snapshot tests

--- tests/index.js
+++ tests/index.js
@@ -1,33 +1,33 @@
 const test = require('ava');
-const vfs = require('vinyl-fs');
-const crypto = require('crypto');
+// const vfs = require('vinyl-fs');
+// const crypto = require('crypto');
 const { promisify } = require('util');
 
 const exec = promisify(require('child_process').exec);
 
-const md5 = str =>
-  crypto
-    .createHash('md5')
-    .update(str)
-    .digest('hex');
+// const md5 = str =>
+//   crypto
+//     .createHash('md5')
+//     .update(str)
+//     .digest('hex');
 
-const hashDirectory = glob =>
-  new Promise((resolve, reject) => {
-    const files = {};
+// const hashDirectory = glob =>
+//   new Promise((resolve, reject) => {
+//     const files = {};
 
-    vfs
-      .src(glob)
-      .on('data', file => {
-        // Transform Vinyl files into a more snapshot-friendly structure.
-        if (!file.contents) {
-          return;
-        }
+//     vfs
+//       .src(glob)
+//       .on('data', file => {
+//         // Transform Vinyl files into a more snapshot-friendly structure.
+//         if (!file.contents) {
+//           return;
+//         }
 
-        files[file.relative] = md5(file.contents.toString());
-      })
-      .on('end', () => resolve(files))
-      .on('error', err => reject(err));
-  });
+//         files[file.relative] = md5(file.contents.toString());
+//       })
+//       .on('end', () => resolve(files))
+//       .on('error', err => reject(err));
+//   });
 
 test.afterEach(() => exec('npm run clean'));
 
@@ -43,8 +43,8 @@ test('build for prod', async t => {
   t.pass();
 });
 
-test('snapshot files in dist', async t => {
-  await exec('npm run build');
-  const distHashes = await hashDirectory('dist/**/*');
-  t.snapshot(distHashes);
-});
+// test('snapshot files in dist', async t => {
+//   await exec('npm run build');
+//   const distHashes = await hashDirectory('dist/**/*');
+//   t.snapshot(distHashes);
+// });
