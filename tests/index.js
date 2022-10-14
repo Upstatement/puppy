cff703ec69d07a04b9f6247df480d9f1e2a3fb17
Mike Burns <mike.burns@upstatement.com>
2022-10-14 13:31:09 -0400

    Update Puppy to v0.12.0, fix failing tests

--- tests/index.js
+++ tests/index.js
@@ -1,18 +1,19 @@
-const test = require('ava');
-const { promisify } = require('util');
+import test from 'ava';
+import { promisify } from 'node:util';
+import { exec } from 'node:child_process';
 
-const exec = promisify(require('child_process').exec);
+const asyncExec = promisify(exec);
 
 test.afterEach(() => exec('npm run clean'));
 
 test('build for dev', async t => {
-  const output = await exec('npm run build:dev');
+  const output = await asyncExec('npm run build:dev');
   t.true(output.stdout.includes('Build Mode: Development'));
   t.pass();
 });
 
 test('build for prod', async t => {
-  const output = await exec('npm run build');
+  const output = await asyncExec('npm run build');
   t.true(output.stdout.includes('Build Mode: Production'));
   t.pass();
 });
